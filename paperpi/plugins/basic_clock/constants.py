bf7ed6b982d3300cf8d996d8cea8dbe12ecd02ff
txoof <aaron.ciuffo@gmail.com>
2020-10-25 22:14:33 +0100

    more backups

---
+++ paperpi/plugins/basic_clock/constants.py
@@ -0,0 +1,5 @@
+version = '0.1.0'
+
+name = 'basic clock'
+
+data = { 'digit_time': '00:00' }
---
+++ paperpi/plugins/lms_client/constants.py
@@ -0,0 +1,15 @@
+version = '0.1.0'
+name = 'lms_client'
+data = {
+        'id': 0,
+        'title': 'Err: No Player',
+        'artist': 'Err: No Player',
+        'coverid': 'Err: No Player',
+        'duration': 0,
+        'album_id': 'Err: No Player',
+        'genre': 'Err: No Player',
+        'album': 'Err: No Player',
+        'artwork_url': 'Err: No Player',
+        'coverart': 'None',
+        'mode': 'None'
+    }
