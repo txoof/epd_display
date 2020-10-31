d927f68831d720804b849c7489ea627d72ed1c62
txoof <aaron.ciuffo@gmail.com>
2020-10-31 17:21:56 +0100

    add attributes for easier testing

--- paperpi/library/SelfDummy.py
+++ paperpi/library/SelfDummy.py
@@ -9,9 +9,12 @@
 class SelfDummy:
     '''dummy class for testing plugins'''
     def __init__(self):
+        config = {}
+        self.name = None
         self.layout = {}
         self.max_priority = 1
         self.min_display_time = 20
         self.refresh_rate = 2
+        self.data = {}
 
 
