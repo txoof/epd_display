8b74b6e81579865a10999ff600ea05c36f93aa2e
txoof <aaron.ciuffo@gmail.com>
2020-11-26 22:49:34 +0100

    Add explicit hidden imports

--- pyinstaller_cfg.py
+++ pyinstaller_cfg.py
@@ -1,10 +1,20 @@
+# base path that contains the project
+base_path = './paperpi'
 # entrance script for program
 base_script = 'paperpi.py'
 
+plugin_path = './paperpi/plugins'
+
 # command additional options
 options = [
-  '-F',
-  ''
+  '--noconfirm',
+  '--clean',
+]
+
+hidden_imports = [
+  'spidev',
+  'RPi',
+  'RPi.GPIO',
 ]
 
 # data files and directories to include
