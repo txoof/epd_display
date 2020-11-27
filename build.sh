38c6534c400d13231b61ea9155cab95d332610df
txoof <aaron.ciuffo@gmail.com>
2020-11-27 13:13:39 +0100

    add build script

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+function check_env {
+  echo "checking build environment"
+  if ! command -v pipenv &> /dev/null
+  then
+    echo "pipenv could not be found"
+    echo "install with `$ apt install pipenv`"
+  else
+    echo "pipenv OK"
+  fi
+
+  ls
+
+  if ! pipenv --venv
+  then
+    echo "setting up pipenv for this project"
+    if ! pipenv update
+    then
+      echo "failed to build pipenv"
+      exit 1
+    fi
+
+  fi
+}
+
+check_env
