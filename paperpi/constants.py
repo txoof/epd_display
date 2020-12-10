d7a1238b0bbdc328c0219988dd5181654ee73e48
txoof <aaron.ciuffo@gmail.com>
2020-12-10 22:05:11 +0100

    bump version

--- paperpi/constants.py
+++ paperpi/constants.py
@@ -3,7 +3,7 @@ from pathlib import Path
 app_name = 'PaperPi'
 contact = 'aaron.ciuffo@gmail.com'
 devel_name = f'com.txoof.{app_name.lower()}'
-version = '0.1.6'
+version = '0.1.6_new_epdlib'
 url = 'https://github.com/ txoof/epd_display'
 
 ## configuration
