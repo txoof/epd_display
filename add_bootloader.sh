fc88b657da260f5081b97c883bf688db6daa4958
txoof <aaron.ciuffo@gmail.com>
2022-01-22 21:01:33 +0100

    add script for creating bootloader if missing

---
+++ add_bootloader.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo "This script adds the Linux0-32bit-arm bootloader (if needed)"
+
+if [ -z $PIPENV_ACTIVE  ];
+then
+  echo "This script must be run from within a pipenv shell"
+  echo "Try: $ pipenv shell"
+  echo "then $ $0"
+  exit 0
+fi
+
+VENV=$(pipenv --venv)
+BOOTLOADER=$(find $VENV -path "*bootloader")
+
+
+if [[ -d $BOOTLOADER/Linux-32bit-arm ]]; 
+then
+  echo "PyInstaller bootloader found -- no action needed"
+  exit 0
+else
+  echo "Creating PyInstaller Bootloader"
+  mkdir ./tmp
+  pushd ./tmp
+  git clone  https://github.com/pyinstaller/pyinstaller
+  cd pyinstaller/bootloader
+  python3 ./waf distclean all
+  cp -R ../PyInstaller/bootloader/Linux-32bit-arm "$BOOTLOADER/Linux-32bit-arm"
+
+
+
+fi
